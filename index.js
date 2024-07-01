6c0559ce593b8be4d7f8d4dd943dd8506570adc1
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2024-07-01 13:35:45 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+introdução REACT
+feito pelo facebook
+react é uma biblioteca javascript paracriação de interface de usuarios
+
+sintaxe do react é chamda de JSX, significa javascript syntax Extension
+consigo misturar codigo javascript com html
+estrutura:
+import React from "react";
+
+function MyCompanint() {
+
+  const message = "Hello World";
+  const paragraph = `é chamdo de JSX e é uma extensão de sintaze javcascript
+  mais infomações ...
+  maisn infomraçõar.....
+  JSX produz elemntos do react`;
+
+  return(
+    <div>
+      <h1>{message}</h1>
+      <p>{paragraph}</p>
+    </div>
+  );
+}
+export default MyComponent
+___________________________________________________________________________________________________________
+ddentro do react utilizandom o consceito de componentes
+podemos utilizar o mesmo componentes varias vezzes ...
+  ]podemos utiilizar css / html ... varias vezes 
+___________________________________________________________________________________________________________
+ANGULAR / VUE.JS
+são outras tecnologias que fazem as mesma coisas!
+___________________________________________________________________________________________________________
+ VAMOS FOCAR NO REACT :
+ EXISTE O REACT NATIVE///// 
+REACT - INTERFACE WEB
+REACT NATIVE - APLICATIVOS / IOS ANDRODE ...
+___________________________________________________________________________________________________________
+NPM gerenciador de pacotes ... conseguimos utilizar express !!!
+alem do npm :
+
+  TEMOS TAMBÉM .. :
+    PNpm / yarn ...
+vai depender muito da ferramenta que a empresa vai utilizar ..
+  ___________________________________________________________________________________________________________
+como já aprendemos com npm, dessa vez vamos utilizar o YARN !!!!!!!!!!!1
+
+PARA INSTALAR O YARN, VAMOS UTILIZAR O NPM !!! HAHAHAHAHAHAHAHAHA
+com npm já vem o NODE!
+1° comando : no terminal DO POWERSHEL (WINDOWNS) caso nçao tenho o powershel, tem que instalar ... psquisa e instala!
+npm install --global yarn    ok  // dessa maneira ela fica disponivel para qualquer parte da nossa aplicação. devido ao (--global)
+
+já está habilitado o uso do yarn
+___________________________________________________________________________________________________________
+vamos iniciar o projeto do zero !
+RECOMENDAÇÃO DO RODOLFO LEIA A DOCUMENTÇÃO DO PROJETO!!!!
+TEM A MANEIRA DE INICIAR O REACT DO ZERO CRIANDO ARQUIVO POR ARQUIVO, MAS VAMOS ECONOZIMAR TEMPO E UTILIZAR O VITE!!!!
+ vite:
+ ELE já prepara nosso projeto para comerçamos a criar nossa aplicação!
+o vite pode ser iniciado pelo npm/yarn/pnpm/bun
+vamos utilizar pelo yarn aqui ...
+  
+ 
